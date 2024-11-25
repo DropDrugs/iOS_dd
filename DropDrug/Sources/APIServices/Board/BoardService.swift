17a9ce3bcf6f50df913c585be9be99e8eb21a743
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-11-25 12:35:31 +0900

    #31 공지사항 API 세팅 중

---
+++ DropDrug/Sources/APIServices/Board/BoardService.swift
@@ -0,0 +1,33 @@
+// Copyright © 2024 RT4. All rights reserved
+
+import Foundation
+import Moya
+
+enum BoardService {
+    case getBoard
+}
+
+extension BoardService: TargetType {
+    var baseURL: URL {
+        guard let url = URL(string: Constants.NetworkManager.baseURL) else {
+            fatalError("fatal error - invalid url")
+        }
+        return url
+    }
+    
+    var path: String {
+        return "members"
+    }
+    
+    var method: Moya.Method {
+        return .get
+    }
+    
+    var task: Task {
+        return .requestPlain
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/json"]
+    }
+}
